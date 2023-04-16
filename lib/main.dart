a8d0dc2085ba6d1988f22077a33c98b3720605b9
Vinicio Borja <75345399+Vinici0@users.noreply.github.com>
2023-04-16 04:10:07 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:estados/pages/pagina1_page.dart';
  
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'pagina1',
-      routes: {
-        'pagina1': ( _ ) => Pagina1Page(),
-        'pagina2': ( _ ) => Pagina2Page(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => UsuarioCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'pagina1',
+        routes: {
+          'pagina1': (_) => Pagina1Page(),
+          'pagina2': (_) => Pagina2Page(),
+        },
+      ),
     );
   }
 }
